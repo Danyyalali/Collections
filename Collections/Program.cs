7f7eb189b59610c2e0d782b402f49e3d22d3c299
Danyyal Ali Yar <danyyal.ali.31@gmail.com>
2021-02-08 10:36:02 +0500

    Added first Hello Message

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -7,6 +7,7 @@ namespace Collections
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello From my Side");
         }
     }
 }
